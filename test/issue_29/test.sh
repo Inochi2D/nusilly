98f5c0d1dab52b4bd543d47d83d78a92b9c23ec3
Carlin St Pierre <carlin.stpierre@gmail.com>
2019-10-27 09:48:45 +1100

    Add another test case to issue 29 test

--- test/issue_29/test.sh
+++ test/issue_29/test.sh
@@ -2,6 +2,7 @@
 
 OUTPUT=$(dub test -b unittest-cov --root=$(dirname "${BASH_SOURCE[0]}") --skip-registry=all --nodeps -q -- --no-colours 2>&1)
 
-echo "$OUTPUT" | grep -c "Summary: 0 passed, 0 failed" > /dev/null
+echo "$OUTPUT" | grep -cv "ordinary case"               > /dev/null
+echo "$OUTPUT" | grep -c  "Summary: 1 passed, 0 failed" > /dev/null
 
 rm -r $(dirname "${BASH_SOURCE[0]}")/.dub $(dirname "${BASH_SOURCE[0]}")/issue_29-test-unittest
