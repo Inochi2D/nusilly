b0970cf62ba7d9d8f4e9bd70a2635f72523d8c23
Anton Fediushin <imantonmeep@gmail.com>
2020-01-06 11:47:54 +0100

    🛠️  Fix #30

--- silly.d
+++ silly.d
@@ -86,7 +86,8 @@ shared static this() {
 
 			// Unittests in structs and classes
 			foreach(member; __traits(derivedMembers, module_))
-				static if(!__traits(isTemplate,  __traits(getMember, module_, member)) &&
+				static if(__traits(compiles, __traits(getMember, module_, member)) &&
+					!__traits(isTemplate,  __traits(getMember, module_, member)) &&
 					__traits(compiles, __traits(parent, __traits(getMember, module_, member))) &&
 					__traits(isSame, __traits(parent, __traits(getMember, module_, member)), module_) &&
 					__traits(compiles, __traits(getUnitTests, __traits(getMember, module_, member))))
