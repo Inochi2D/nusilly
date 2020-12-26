25c6dfd7aacbb3053249c53d63bb22769c35685e
Carlin St Pierre <carlin.stpierre@mailbox.org>
2020-12-26 17:18:24 +1100

    Update test expectations for issue 29

--- test/issue_29/test.sh
+++ test/issue_29/test.sh
@@ -3,6 +3,6 @@
 OUTPUT=$(dub test -b unittest-cov --root=$(dirname "${BASH_SOURCE[0]}") --skip-registry=all --nodeps -q -- --no-colours 2>&1)
 
 echo "$OUTPUT" | grep -c  "✓ issue_29 ordinary case"    > /dev/null
-echo "$OUTPUT" | grep -c  "Summary: 1 passed, 0 failed" > /dev/null
+echo "$OUTPUT" | grep -c  "Summary: 2 passed, 0 failed" > /dev/null
 
 rm -r $(dirname "${BASH_SOURCE[0]}")/.dub $(dirname "${BASH_SOURCE[0]}")/issue_29-test-unittest
